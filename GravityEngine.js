47aee619fe210401d121597f48cc6804e0eaeec5
KenG98 <kengarber98@gmail.com>
2015-04-04 14:19:28 -0400

    Ready to add movement
    Displays (sort of) correctly (can’t see the orb yet for some reason).
    Time to make things move and hopefully i’ll see whats wrong with the
    orb.

--- GravityEngine.js
+++ GravityEngine.js
@@ -1,18 +1,40 @@
+//scene and camera
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
+//renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMapEnabled = true;
 document.body.appendChild(renderer.domElement);
 
-var gs = new GravityCenter(0,0,0);
-scene.add(gs.gravityBody);
 
-var oneOrb = new Orb(100,100,0,1,1,1);
-scene.add(oneOrb.orbBody);
+//gravity center
+var gravityPosition = {x:0, y:0, z:0};
+var gravityPull = 0.1;
+var gravityGeometry = new THREE.SphereGeometry(30);
+var gravityMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+var gravityBody = new THREE.Mesh(gravityGeometry, gravityMaterial);
+gravityBody.castShadow = true;
+gravityBody.receiveShadow = true;
+gravityBody.position = gravityPosition;
+scene.add(gravityBody);
 
-//light (for shadows)
+
+//orb
+var orbPosition = {x:100, y:100, z: 100};
+var orbForce = {x:1, y:1, z: 1};
+var orbGeometry = new THREE.SphereGeometry(10);
+var orbMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00});
+var orbBody = new THREE.Mesh(orbGeometry, orbMaterial);
+orbBody.castShadow = true;
+orbBody.receiveShadow = true;
+orbBody.position = orbPosition;
+scene.add(orbBody);
+
+
+//spotlight
 var spotLight = new THREE.SpotLight( 0xffffff );
 spotLight.position.set( 0, 1000, 0 );
 spotLight.castShadow = true;
@@ -20,10 +42,8 @@ spotLight.shadowCameraNear = 500;
 spotLight.shadowCameraFar = 2000;
 spotLight.shadowCameraFov = 50;
 scene.add( spotLight );
-
 //amvient light
-var ambientLight = new THREE.AmbientLight( 0x555555 ); // soft white light
-// var ambientLight = new THREE.AmbientLight( 0xffffff );
+var ambientLight = new THREE.AmbientLight( 0x555555 );
 scene.add( ambientLight );
 
 camera.position.x = 300;
@@ -32,6 +52,7 @@ camera.position.z = 300;
 camera.lookAt({x:0,y:0,z:0});
 
 function render(){
+	//move the orb
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }
